30d76f103ff6daa662e18c98f566a520b4e9734e
Gihzx <145726882+Gihzx@users.noreply.github.com>
2024-09-27 23:20:24 -0300

    feat: Atomic  Desing para a organização dos componentes, navbar e responsividade feita

---
+++ frontEnd/src/Routes.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default AppRoutes;
