60097e6238428d3783bd120306113ce6c8823eeb
Paulo Santos <paulo.santos@nutes.ufpe.br>
2024-11-18 22:48:59 -0300

    modify routes.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,10 @@
 import { NavBar } from "../../components/molecules/navBar/NavBar";
 
-import Actions from "../../components/molecules/projectActions/actions";
+import Actions from "../../components/molecules/projectActions/Actions";
 import VolunteerCountComponent from "../../components/molecules/VolunteerCountComponent/VolunteerCountComponent";
-import HistoryOng from "../../components/molecules/historyOng/historyOng";
-import VideoYoutube from "../../components/molecules/videoYoutube/videoYoutube";
-import UserContact from "../../components/molecules/userContact/userContact";
+import HistoryOng from "../../components/molecules/historyOng/HistoryOng";
+import VideoYoutube from "../../components/molecules/videoYoutube/VideoYoutube";
+import UserContact from "../../components/molecules/userContact/UserContact";
 import Testimony from "../../components/molecules/testimony/Testimony";
 import Footer from "../../components/molecules/footer/Footer";
 import Carousel from "../../components/molecules/carousel/Carousel";
