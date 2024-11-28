b3706b2432d6a30f3b941d405ee2445c0a4408f9
Paulo Santos <paulo.santos@nutes.ufpe.br>
2024-11-28 00:00:49 -0300

    Get Functions

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import { NavBar } from "../../components/molecules/navBar/NavBar";
 import HistoryOngHome from "../../components/molecules/historyOngHome/HistoryOngHome";
 import VolunteerCountComponent from "../../components/molecules/VolunteerCountComponent/VolunteerCountComponent";
 import VideoYoutube from "../../components/molecules/videoYoutube/VideoYoutube";
-import UserContact from "../../components/molecules/userContact/userContact";
+import UserContact from "../../components/molecules/userContact/UserContact";
 import Testimony from "../../components/molecules/testimony/Testimony";
 import Footer from "../../components/molecules/footer/Footer";
 import Carousel from "../../components/molecules/carousel/Carousel";
